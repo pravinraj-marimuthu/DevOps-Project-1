c19d2d9b144813d5cfe6c00dc2880c9b78466c71
root <root@ip-172-31-33-199.ap-south-1.compute.internal>
2024-10-24 11:16:24 +0000

    ee

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker-compose down -f /opt/devops-build/build/compose.yaml
+docker compose down -f docker-compose.custom.yml logs --follow
 docker-compose up -d
 docker login --username pravinrajmarimuthu --password Iampravin7010
 docker push pravinrajmarimuthu/my-react-app-dev:latest
