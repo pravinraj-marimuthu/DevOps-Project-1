5a9fd26f252b6ac4f757d366fc79cfef1e953841
root <root@ip-172-31-33-199.ap-south-1.compute.internal>
2024-10-24 11:06:39 +0000

    script change

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker compose down
-docker compose up -d
+docker-compose down
+docker-compose up -d
 docker login --username pravinrajmarimuthu --password Iampravin7010
 docker push pravinrajmarimuthu/my-react-app-dev:latest
