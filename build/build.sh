08d820f6df0cc5bc8ef4b5026ab36443b8912766
root <root@ip-172-31-33-199.ap-south-1.compute.internal>
2024-10-24 11:43:11 +0000

    gommala

--- build/build.sh
+++ build/build.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 
-# Initialize the version (if not already set)
-if [ -z "$IMAGE_VERSION" ]; then
-    IMAGE_VERSION=1
-fi
-
-# Your other docker-related commands go here
-
-echo "Re-building thee image"
+echo "Re-building the Docker Image"
+echo ""
+echo ""
 docker-compose -f /opt/devops-build/build/compose.yaml down
 docker-compose -f /opt/devops-build/build/compose.yaml up -d
 echo "Pushing the image to Docker Hub"
+echo ""
+echo ""
 docker login --username pravinrajmarimuthu --password Iampravin7010
-docker push pravinrajmarimuthu/my-react-app-dev:$IMAGE_VERSION
-
-# Increment the version for the next run
-((IMAGE_VERSION++))
-
-# Optionally, display the new version
-echo "Image version is now $IMAGE_VERSION"
+docker push pravinrajmarimuthu/my-react-app-dev:latest
