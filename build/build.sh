072f1af087b53361ffd01de80de04207be9dde57
root <root@ip-172-31-33-199.ap-south-1.compute.internal>
2024-10-24 14:37:43 +0000

    commit final

--- build/build.sh
+++ build/build.sh
@@ -5,6 +5,11 @@ echo "Building the latest Docker Image..."
 echo ""
 echo ""
 docker-compose -f /opt/devops-build/build/compose.yaml down
+echo ""
+echo ""
+docker-compose -f /opt/devops-build/build/compose.yaml build
+echo ""
+echo ""
 docker-compose -f /opt/devops-build/build/compose.yaml up -d
 echo ""
 echo ""
@@ -12,9 +17,12 @@ echo "Pushing the image to Docker Hub..."
 echo ""
 echo ""
 docker login --username pravinrajmarimuthu --password Iampravin7010
+echo ""
+echo ""
 docker push pravinrajmarimuthu/my-react-app-dev:latest
 echo ""
 echo ""
 echo "The docker image has been successfully pushed...!"
 echo ""
 echo ""
+docker ps
