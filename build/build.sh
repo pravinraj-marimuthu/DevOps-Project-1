0ab3116c3c7e8d9d702b2d738a4110fee5ae7074
root <root@ip-172-31-33-199.ap-south-1.compute.internal>
2024-10-24 17:18:00 +0000

    Last commit

--- build/build.sh
+++ build/build.sh
@@ -2,29 +2,20 @@
 timestamp_tag=$(date +%Y%m%d%H%M%S)
 sudo echo $timestamp_tag > /opt/devops-build/build/versions.txt
 echo ""
-echo ""
 echo "Building the latest Docker Image..."
-echo ""
 echo ""
 docker-compose -f /opt/devops-build/build/compose.yaml down
 echo ""
-echo ""
 docker-compose -f /opt/devops-build/build/compose.yaml build
-echo ""
 echo ""
 docker tag build-frontend pravinrajmarimuthu/my-react-app-dev:$(cat /opt/devops-build/build/versions.txt)
 echo ""
-echo ""
 echo "Pushing the image to Docker Hub..."
-echo ""
 echo ""
 docker login --username pravinrajmarimuthu --password Iampravin7010
 echo ""
-echo ""
 docker push pravinrajmarimuthu/my-react-app-dev:$(cat /opt/devops-build/build/versions.txt)
 echo ""
-echo ""
 echo "The latest Docker image version is $(cat /opt/devops-build/build/versions.txt)"
-echo ""
 echo ""
 echo "The docker image has been successfully built and pushed...!"
--- build/deploy.sh
+++ build/deploy.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
 echo ""
-echo ""
 docker login
-echo ""
 echo ""
 docker pull pravinrajmarimuthu/my-react-app-dev:$(cat /opt/devops-build/build/versions.txt )
 echo ""
-echo ""
 echo "The pulled docker image version is $(cat /opt/devops-build/build/versions.txt )"
 echo ""
-echo ""
 docker-compose -f /opt/devops-build/build/compose.yaml up -d
-echo ""
 echo ""
 docker ps
