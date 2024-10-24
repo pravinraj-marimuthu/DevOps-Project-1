09e94a9390c5562831fb34b90ec5d48aac812314
root <root@ip-172-31-33-199.ap-south-1.compute.internal>
2024-10-24 16:18:18 +0000

    final

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh	
 timestamp_tag=$(date +%Y%m%d%H%M%S)
-export docker_image_tag=$timestamp_tag
-echo "$docker_image_tag" > /opt/devops-build/build/versions.txt
+echo $timestamp_tag > /opt/devops-build/build/versions.txt
 echo ""
 echo ""
 echo "Building the latest Docker Image..."
@@ -13,7 +12,7 @@ echo ""
 docker-compose -f /opt/devops-build/build/compose.yaml build
 echo ""
 echo ""
-docker tag build-frontend pravinrajmarimuthu/my-react-app-dev:$docker_image_tag
+docker tag build-frontend pravinrajmarimuthu/my-react-app-dev:$(cat /opt/devops-build/build/versions.txt)
 echo ""
 echo ""
 echo "Pushing the image to Docker Hub..."
@@ -22,10 +21,10 @@ echo ""
 docker login --username pravinrajmarimuthu --password Iampravin7010
 echo ""
 echo ""
-docker push pravinrajmarimuthu/my-react-app-dev:$docker_image_tag
+docker push pravinrajmarimuthu/my-react-app-dev:$(cat /opt/devops-build/build/versions.txt)
 echo ""
 echo ""
-echo "The latest Docker image version is $docker_image_tag"
+echo "The latest Docker image version is $(cat /opt/devops-build/build/versions.txt)"
 echo ""
 echo ""
 echo "The docker image has been successfully built and pushed...!"
