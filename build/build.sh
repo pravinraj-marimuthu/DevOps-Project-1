f7e16ecf0d6f22ef857c34297d86c556ffaf3202
root <root@ip-172-31-33-199.ap-south-1.compute.internal>
2024-10-24 15:04:45 +0000

    final

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+timestamp_tag=$(date +%Y%m%d%H%M%S)
 echo ""
 echo ""
 echo "Building the latest Docker Image..."
@@ -19,7 +20,7 @@ echo ""
 docker login --username pravinrajmarimuthu --password Iampravin7010
 echo ""
 echo ""
-docker push pravinrajmarimuthu/my-react-app-dev:latest
+docker push pravinrajmarimuthu/my-react-app-dev:$timestamp_tag
 echo ""
 echo ""
 echo "The docker image has been successfully pushed...!"
